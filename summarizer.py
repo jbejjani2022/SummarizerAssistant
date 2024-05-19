9c844d1e15e62c44d185894c62f44b6d3ae429e8
jbejjani2022 <51431082+jbejjani2022@users.noreply.github.com>
2024-05-19 15:00:35 -0400

    increase token limit

--- summarizer.py
+++ summarizer.py
@@ -1,11 +1,12 @@
-# Usage: python summarizer.py -t/--type {'n', 'mr'} text
-# ex. python summarizer.py -t mr https://en.wikipedia.org/wiki/Ludwig_van_Beethoven
+# Usage: python summarizer.py -t/--type {'n', 'm', 'c'} text
+# ex. python summarizer.py -t m https://en.wikipedia.org/wiki/Ludwig_van_Beethoven
 # python summarizer.py -t n data/romeojuliet.txt
 # where text is a string, .txt file, or URL
-# 'n' specifies naive summarization strategy, 'mr' specifies map_reduce strategy
+# 'n' specifies naive summarization strategy, 'm' specifies map_reduce strategy
 
 import naive_summarizer
 import map_reduce_summarizer
+import chunk_summarizer
 import argparse
 
 if __name__ == "__main__":
@@ -13,14 +14,16 @@ if __name__ == "__main__":
                     prog='summarizer',
                     description='Summarize an input string, the contents of a .txt file, or a web page given its URL')
     
-    parser.add_argument('-t', '--type', required=True, choices=['n', 'mr'],
-                        help='specify the summarization type: -n for naive truncation, -mr for map_reduce')
+    parser.add_argument('-t', '--type', required=True, choices=['n', 'm', 'c'],
+                        help='specify the summarization type: -n for naive truncation, -m for map_reduce, -c for chunking')
     parser.add_argument('text', help='a string, .txt file, or URL to be summarized')
     args = parser.parse_args()
 
     text = args.text
     if args.type == 'n':
         summary = naive_summarizer.summarize(text)
-    elif args.type == 'mr':
+    elif args.type == 'm':
         summary = map_reduce_summarizer.summarize(text)
+    elif args.type == 'c':
+        summary = chunk_summarizer.summarize(text)
     print(summary)
